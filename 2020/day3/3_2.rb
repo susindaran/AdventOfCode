5c80f3b602660235da370da50e77c51e354f2472
Susindaran Elangovan <susindaran94@gmail.com>
2020-12-03 23:25:51 -0800

    Refactor day 3 solutions to use template

--- 2020/day3/3_2.rb
+++ 2020/day3/3_2.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+require_relative '../../aoc'
+
 def trees_encountered(input)
   slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
   map = input.map(&:chars)
@@ -21,8 +25,8 @@ def trees_encountered(input)
   total_trees
 end
 
-# read input
-filename = File.basename(__FILE__).split('.')[0]
-file = File.open("#{filename}.input")
-input = file.readlines.map(&:chomp)
-puts trees_encountered(input)
+input_file_name = File.basename(__FILE__).split('.')[0]
+AOC.problem input_file_name do |input|
+  input = input.map(&:chomp)
+  trees_encountered(input)
+end
