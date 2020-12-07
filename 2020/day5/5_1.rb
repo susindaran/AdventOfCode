446d69660d96641b87bc81006f63806d4c4a7953
Susindaran Elangovan <susindaran94@gmail.com>
2020-12-07 14:40:10 -0800

    Optimize day 5 solutions

--- 2020/day5/5_1.rb
+++ 2020/day5/5_1.rb
@@ -2,26 +2,13 @@
 
 require_relative '../../aoc'
 
-def get_row(seating)
-  seating[0..6].reverse.chars.each_with_index.inject(0) do |row, (dir, index)|
-    row | ((dir == 'F' ? 0 : 1) << index)
-  end
-end
-
-def get_column(seating)
-  seating[7..9].reverse.chars.each_with_index.inject(0) do |col, (dir, index)|
-    col | ((dir == 'R' ? 1 : 0) << index)
-  end
-end
-
 def parse_seating(seating)
-  get_row(seating) * 8 + get_column(seating)
+  seating = seating.gsub('F', '0').gsub('B', '1').gsub('R', '1').gsub('L', '0')
+  seating[0..6].to_i(2) * 8 + seating[7..9].to_i(2)
 end
 
 def highest_seat(seatings)
-  seatings.map do |seating|
-    parse_seating(seating)
-  end.max
+  seatings.map { |seating| parse_seating(seating) }.max
 end
 
 input_file_name = File.basename(__FILE__).split('.')[0]
--- 2020/day5/5_2.rb
+++ 2020/day5/5_2.rb
@@ -2,20 +2,9 @@
 
 require_relative '../../aoc'
 
-def get_row(seating)
-  seating[0..6].reverse.chars.each_with_index.inject(0) do |row, (dir, index)|
-    row | ((dir == 'F' ? 0 : 1) << index)
-  end
-end
-
-def get_column(seating)
-  seating[7..9].reverse.chars.each_with_index.inject(0) do |col, (dir, index)|
-    col | ((dir == 'R' ? 1 : 0) << index)
-  end
-end
-
 def parse_seating(seating)
-  get_row(seating) * 8 + get_column(seating)
+  seating = seating.gsub('F', '0').gsub('B', '1').gsub('R', '1').gsub('L', '0')
+  seating[0..6].to_i(2) * 8 + seating[7..9].to_i(2)
 end
 
 def missing_seat_id(seatings)
