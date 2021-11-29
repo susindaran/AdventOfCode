4a82de0107706e50aa67243bcfb4b5bfcf9abcc3
Susindaran Elangovan <susindaran94@gmail.com>
2021-11-29 14:57:36 -0800

    Day 19

---
+++ 2020/day19/19_1.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative '../../aoc'
+
+def resolve_rule(id, rules)
+  value = rules[id]
+  return value if value.match(/^[a-z]+$/)
+
+  parts = value.split('|').map(&:strip)
+
+  resolved = parts.map do |part|
+    ids = part.split(' ')
+    ids.map do |char|
+      resolve_rule(char, rules)
+    end.join('')
+  end.join('|')
+
+  resolved = '(' + resolved + ')' if parts.length > 1
+  resolved
+end
+
+def find_matching(rules, messages)
+  rules = rules.map { |rule| rule.split(':') }.to_h
+  resolved = '^' + resolve_rule('0', rules) + '$'
+
+  messages.map { |msg| msg.match(resolved) ? 1 : 0 }.sum
+end
+
+AOC.problem do |input|
+  rules, messages = input.join('').split("\n\n")
+  rules = rules.gsub('"', '').gsub(': ', ':').split("\n")
+  messages = messages.split("\n")
+  find_matching(rules, messages)
+end
