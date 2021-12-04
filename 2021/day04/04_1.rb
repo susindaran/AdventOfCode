e963c02f7ebcc389112df13c9b54e9c273a38fa6
Susindaran Elangovan <susindaran94@gmail.com>
2021-12-04 15:02:52 -0800

    2021 - Day 04

---
+++ 2021/day04/04_1.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require_relative '../../aoc'
+
+class Cell
+  attr_reader :val, :row, :col, :marked
+  def initialize(val, row, col)
+    @val = val
+    @row = row
+    @col = col
+    @marked = false
+  end
+
+  def mark
+    @marked = true
+  end
+end
+
+class Board
+  def initialize(grid)
+    @grid = grid
+  end
+
+  def win?
+    @grid.any? { |row| row.all?(&:marked) } ||
+      @grid.transpose.any? { |col| col.all?(&:marked) }
+  end
+
+  def score
+    @grid.map { |row| row.map { |cell| cell.marked ? 0 : cell.val }.sum }.sum
+  end
+
+  def self.build(lines, number_map)
+    grid = lines.split("\n").map.each_with_index do |line, row|
+      line.split(' ').map.each_with_index do |val, col|
+        val = val.strip.to_i
+        cell = Cell.new(val, row, col)
+        number_map[val] << cell
+        cell
+      end
+    end
+
+    Board.new(grid)
+  end
+end
+
+AOC.problem(read_lines: false) do |lines|
+  parts = lines.split("\n\n")
+  numbers = parts[0].split(',').map(&:to_i)
+
+  number_map = Hash.new { |h, k| h[k] = [] }
+  boards = parts[1..].map { |part| Board.build(part, number_map) }
+
+  winner_score = nil
+  numbers.each do |num|
+    number_map[num].each(&:mark)
+    boards.each do |board|
+      next unless board.win?
+
+      winner_score = board.score * num
+      break
+    end
+
+    break unless winner_score.nil?
+  end
+
+  winner_score
+end
--- aoc.rb
+++ aoc.rb
@@ -2,9 +2,13 @@
 
 module AOC
   # Reads all the lines of the input and yields to the block
-  def self.problem(input_file = 'input')
-    file = File.open(input_file)
-    input = file.readlines
+  def self.problem(input_file: 'input', read_lines: true)
+    if read_lines
+      file = File.open(input_file)
+      input = file.readlines
+    else
+      input = File.read(input_file)
+    end
     puts yield(input)
   end
 end
