bda33eda43b7927c094b4ace1c159333e76ea9fa
Susindaran Elangovan <susindaran94@gmail.com>
2022-01-04 14:38:06 -0800

    2021 - Day 11

---
+++ 2021/day11/11_2.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'pry'
+require_relative '../../aoc'
+
+DIRS = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]].freeze
+
+def propogate_flash(mat, pos)
+  mat[pos] = 0
+
+  DIRS.each_with_object([]) do |dir, to_flash|
+    neighbour = [pos[0] + dir[0], pos[1] + dir[1]]
+    val = mat[neighbour]
+    unless val.nil? || val.zero?
+      mat[neighbour] = val + 1
+      to_flash << neighbour if val == 9
+    end
+
+    to_flash
+  end
+end
+
+AOC.problem do |input|
+  # Convert to hash[[row, col]] = val
+  mat = input.each_with_index.map do |line, row|
+    line.strip.chars.each_with_index.map { |e, col| [[row, col], e.to_i] }.to_h
+  end.reduce(:merge)
+
+  flashes = 0
+  step = 0
+  until flashes == 100
+    step += 1
+    flashes = 0
+    mat.transform_values! { |v| v + 1 }
+
+    to_flash = mat.keys.select { |k| mat[k] > 9 }
+
+    until to_flash.empty?
+      to_flash += propogate_flash(mat, to_flash.pop)
+      flashes += 1
+    end
+  end
+
+  step
+end
