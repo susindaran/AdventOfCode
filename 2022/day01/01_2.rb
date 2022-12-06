d8cb706decd02db617824977695b90cda758e673
Susindaran Elangovan <susindaran94@gmail.com>
2022-12-05 21:45:46 -0800

    2022 - Day 01

---
+++ 2022/day01/01_2.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative '../../aoc'
+
+AOC.problem(read_lines: false) do |input|
+  input.split("\n\n").map { |line| line.split("\n").map(&:to_i).sum }.max(3).sum
+end
