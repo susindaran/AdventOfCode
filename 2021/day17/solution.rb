edd01d7cb2f73e0d58f5a5ac7d1d3bd5774fe760
Susindaran Elangovan <susindaran94@gmail.com>
2024-09-30 14:26:41 -0700

    Use new single solution file format with solution validation for 2021/17

---
+++ 2021/day17/solution.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require_relative '../../aoc'
+
+def parse_input(input)
+  regex = /: x=(\d+)..(\d+), y=([-0-9]+)..([-0-9]+)/
+  match = regex.match(input)
+  [match[1].to_i, match[2].to_i, match[3].to_i, match[4].to_i]
+end
+
+AOC.part1(read_lines: false) do |input|
+  _, _, y_start, y_end = parse_input(input)
+
+  lowest = [y_start, y_end].min.abs
+  (lowest * (lowest - 1)) / 2
+end
+
+AOC.part2(read_lines: false) do |input|
+  x_start, x_end, y_start, y_end = parse_input(input)
+
+  res = 0
+  (y_start..y_start.abs).each do |y_vel_seed|
+    (0..x_end).each do |x_vel_seed|
+      x_vel = x_vel_seed
+      y_vel = y_vel_seed
+
+      x, y = 0, 0
+      loop do
+        x, y = x + x_vel, y + y_vel
+        x_vel -= 1 if x_vel.positive?
+        y_vel -= 1
+
+        break if x > x_end || y < y_start
+        next if x < x_start || y > y_end
+
+        res += 1
+        break
+      end
+    end
+  end
+
+  res
+end
+
+AOC.validate_solution do |part1_sol, part2_sol|
+  raise if part1_sol != 5886
+  raise if part2_sol != 1806
+end
--- aoc.rb
+++ aoc.rb
@@ -11,4 +11,41 @@ module AOC
     end
     puts yield(input)
   end
+
+  def self.part1(input_file: 'input', read_lines: true, &blk)
+    @part1 = lambda do
+      if read_lines
+        file = File.open(input_file)
+        input = file.readlines
+      else
+        input = File.read(input_file)
+      end
+      blk.call(input)
+    end
+  end
+
+  def self.part2(input_file: 'input', read_lines: true, &blk)
+    @part2 = lambda do
+      if read_lines
+        file = File.open(input_file)
+        input = file.readlines
+      else
+        input = File.read(input_file)
+      end
+      blk.call(input)
+    end
+  end
+
+  def self.solve
+    part1_sol = @part1&.call
+    part2_sol = @part2&.call
+    puts "Part 1: #{part1_sol}"
+    puts "Part 2: #{part2_sol}"
+
+    [part1_sol, part2_sol]
+  end
+
+  def self.validate_solution
+    yield(*solve)
+  end
 end
